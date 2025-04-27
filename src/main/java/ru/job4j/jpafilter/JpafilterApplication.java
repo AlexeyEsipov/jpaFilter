b45673a8a6ba13f4b809b8bce01f6dcca42680a4
AlexeyEsipov <a_esipov@list.ru>
2025-04-27 16:38:01 +0300

    Исправил ошибки сборки

---
+++ src/main/java/ru/job4j/jpafilter/JpafilterApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.jpafilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpafilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpafilterApplication.class, args);
+    }
+
+}
